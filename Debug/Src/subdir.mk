e38f1f4c623ebe707f495393f6d7278b48b3406e
Yin-1206 <fufuhappy@gmail.com>
2026-02-05 03:33:59 +0800

    Interrupt -> ver1.

--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -5,17 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/GPIO_Polling.c \
+../Src/GPIO_interrupt.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/GPIO_Polling.o \
+./Src/GPIO_interrupt.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/GPIO_Polling.d \
+./Src/GPIO_interrupt.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
@@ -27,7 +27,7 @@ Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/GPIO_Polling.cyclo ./Src/GPIO_Polling.d ./Src/GPIO_Polling.o ./Src/GPIO_Polling.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/GPIO_interrupt.cyclo ./Src/GPIO_interrupt.d ./Src/GPIO_interrupt.o ./Src/GPIO_interrupt.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
 
 .PHONY: clean-Src
 
--- Src/GPIO_interrupt.c
+++ Src/GPIO_interrupt.c
@@ -4,34 +4,29 @@
 
 #define HIGH        1
 
-// 中斷處理函數原型
+void ConfigGPIOLed1(GPIO_Handle_t *pGPIOLed1); // 配置 PD12 LED (push-pull)
+void ConfigGPIOLed2(GPIO_Handle_t *pGPIOLed2); // 配置 PD13 LED (push-pull)
+void ConfigGPIOBtn(GPIO_Handle_t *pGPIOBtn); // 配置 PA0 按鈕 (負緣觸發)
 void EXTI0_IRQHandler(void);
-
 void delay(void);
 
 int main(void){
 
-    GPIO_Handle_t GPIOLed, GPIOBtn;
+    GPIO_Handle_t GPIOLed1, GPIOLed2, GPIOBtn;
 
-    // LED 配置 (PD12 - 推挽輸出)
-    GPIOLed.pGPIOx = GPIOD;
-    GPIOLed.GPIO_PinConfig.GPIO_PinNumber      = GPIO_PIN_NO_12;
-    GPIOLed.GPIO_PinConfig.GPIO_PinMode        = GPIO_MODE_OUT;
-    GPIOLed.GPIO_PinConfig.GPIO_PinSpeed       = GPIO_SPD_VHIGH;
-    GPIOLed.GPIO_PinConfig.GPIO_PinOPType      = GPIO_OP_TYPE_PP;
-    GPIOLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	ConfigGPIOLed1(&GPIOLed1);
+	ConfigGPIOLed2(&GPIOLed2);
 
-    // 按鈕配置 (PA0 - 輸入 + 中斷下降沿)
-    GPIOBtn.pGPIOx = GPIOA;
-    GPIOBtn.GPIO_PinConfig.GPIO_PinNumber      = GPIO_PIN_NO_0;
-    GPIOBtn.GPIO_PinConfig.GPIO_PinMode        = GPIO_MODE_IT_FT;  // 下降沿中斷
-    GPIOBtn.GPIO_PinConfig.GPIO_PinSpeed       = GPIO_SPD_VHIGH;
-    GPIOBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PU;          // 內部上拉
-    GPIOBtn.GPIO_PinConfig.GPIO_PinAltFunMode  = 0;
+	ConfigGPIOBtn(&GPIOBtn);
 
-    // 初始化
-    GPIO_Init(&GPIOLed);
-    GPIO_Init(&GPIOBtn);
+	GPIO_PeriClockControl(GPIOD, ENABLE); // 將GPIO portD 對應bus上clock啟用
+
+	GPIO_Init(&GPIOLed1); // 初始化 PD12 LED
+	GPIO_Init(&GPIOLed2); // 初始化 PD13 LED
+
+	GPIO_PeriClockControl(GPIOA, ENABLE); // 將GPIO portA 對應bus上clock啟用
+
+	GPIO_Init(&GPIOBtn); // 初始化 PA0 按鈕
 
     // 配置 EXTI0 中斷 (IRQ6)
     GPIO_IRQInterruptConfig(6, ENABLE);      // EXTI0 中斷線 IRQ6
@@ -40,8 +35,15 @@ int main(void){
     // 啟用全域中斷
     __asm("cpsie i");
 
+    uint32_t blink_counter = 0;
+
     while (1){
-        // 主迴圈空轉，中斷處理 LED 切換
+    	// 主迴圈負責 PD13 的閃爍任務
+        blink_counter++;
+    	if (blink_counter >= 1000000) {
+    	    GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_13);
+    	    blink_counter = 0;
+        }
     }
 
     return 0;
@@ -49,7 +51,7 @@ int main(void){
 
 // EXTI0 中斷處理函數 (PA0 按下)
 void EXTI0_IRQHandler(void){
-    delay();  // 簡單防抖
+    delay();
 
     // 切換 LED 狀態
     GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
@@ -58,7 +60,47 @@ void EXTI0_IRQHandler(void){
     GPIO_IRQHandling(GPIO_PIN_NO_0);
 }
 
-// 延遲函數 (防抖用)
+void ConfigGPIOLed1(GPIO_Handle_t *pGPIOLed1){
+	//push-pull (PD12 LED)
+	pGPIOLed1->pGPIOx = GPIOD;
+	pGPIOLed1->GPIO_PinConfig.GPIO_PinNumber      = GPIO_PIN_NO_12;
+	pGPIOLed1->GPIO_PinConfig.GPIO_PinMode        = GPIO_MODE_OUT;
+	pGPIOLed1->GPIO_PinConfig.GPIO_PinSpeed       = GPIO_SPD_VHIGH;
+	pGPIOLed1->GPIO_PinConfig.GPIO_PinOPType      = GPIO_OP_TYPE_PP;
+	pGPIOLed1->GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	pGPIOLed1->GPIO_PinConfig.GPIO_PinAltFunMode  = 0;
+
+	//open-drain (若使用內部上拉電阻=>電阻太大，會使LED無法閃爍，因此需要外接一個外部電阻)
+//	pGPIOLed1->pGPIOx = GPIOD;
+//	pGPIOLed1->GPIO_PinConfig.GPIO_PinNumber      = GPIO_PIN_NO_12;
+//	pGPIOLed1->GPIO_PinConfig.GPIO_PinMode        = GPIO_MODE_OUT;
+//	pGPIOLed1->GPIO_PinConfig.GPIO_PinSpeed       = GPIO_SPD_VHIGH;
+//	pGPIOLed1->GPIO_PinConfig.GPIO_PinOPType      = GPIO_OP_TYPE_OD;
+//	pGPIOLed1->GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PU;
+//	pGPIOLed1->GPIO_PinConfig.GPIO_PinAltFunMode  = 0;
+}
+
+void ConfigGPIOLed2(GPIO_Handle_t *pGPIOLed2){
+	//push-pull (PD13 LED)
+	pGPIOLed2->pGPIOx = GPIOD;
+	pGPIOLed2->GPIO_PinConfig.GPIO_PinNumber      = GPIO_PIN_NO_13;
+	pGPIOLed2->GPIO_PinConfig.GPIO_PinMode        = GPIO_MODE_OUT;
+	pGPIOLed2->GPIO_PinConfig.GPIO_PinSpeed       = GPIO_SPD_VHIGH;
+	pGPIOLed2->GPIO_PinConfig.GPIO_PinOPType      = GPIO_OP_TYPE_PP;
+	pGPIOLed2->GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	pGPIOLed2->GPIO_PinConfig.GPIO_PinAltFunMode  = 0;
+}
+
+void ConfigGPIOBtn(GPIO_Handle_t *pGPIOBtn){
+	// 按鈕配置
+    pGPIOBtn->pGPIOx = GPIOA;
+    pGPIOBtn->GPIO_PinConfig.GPIO_PinNumber      = GPIO_PIN_NO_0;
+    pGPIOBtn->GPIO_PinConfig.GPIO_PinMode        = GPIO_MODE_IT_RT;  // 正緣觸發中斷事件(按下瞬間觸發)
+    pGPIOBtn->GPIO_PinConfig.GPIO_PinSpeed       = GPIO_SPD_VHIGH;
+    pGPIOBtn->GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;          // 內部上拉電阻
+    pGPIOBtn->GPIO_PinConfig.GPIO_PinAltFunMode  = 0;
+}
+
 void delay(void){
-    for (uint32_t i = 0; i < 50000; i++);
+    for (volatile uint32_t i = 0; i < 250000; i++);
 }
